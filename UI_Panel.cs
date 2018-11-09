300f5c68959386ae859e8ef98adc357af48b9e9e
esimin <dev.simin@gmail.com>
2018-11-09 15:05:23 +0900

    TestCommit01

---
+++ UI_Panel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Panel : MonoBehaviour {
+
+	// Use this for initialization
+	public SwipeSystem swipeControls;
+	public Animator Panel01;
+
+	private void Update()
+	{
+		if(swipeControls.SwipeDown)
+			Panel01.SetBool("open",true);
+		if(swipeControls.SwipeUp)
+			Panel01.SetBool("open",false);
+	}
+}
